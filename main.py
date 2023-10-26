8f92984a543d55a7b5dc7b5d382cb70c0d6fbdf7
shield <enzox1234567890@gmail.com>
2023-10-26 16:16:34 -0300

    added main (todos) and an example of the usage of PLY

---
+++ main.py
@@ -0,0 +1,17 @@
+import ply
+
+# todo: adicionar palavras reservadas
+
+# todo: adicionar lista dos nomes dos tokens
+
+# todo: adicionar regras com expressoes regulares para identificação de tokens
+
+# todo: adicionar funcoes de achar linhas e colunas
+
+# todo: adicionar regra para ignorar espacos e tabs
+
+# todo : adicionar regra para tratar erros lexicos
+
+# todo: ler arquivo txt dos dados
+
+# todo: percorrer o arquivo, ler os tokens, e preencher a tabela de simbolos

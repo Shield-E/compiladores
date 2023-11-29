18dc4bcfb9d86824a75ae2f16acb2be1e95e981b
André Fernandes <fpf.andre@gmail.com>
2023-11-28 22:26:43 -0300

    simplify semantic rules using a global variable counter

--- compila/test_lang/test_lang_grammar.py
+++ compila/test_lang/test_lang_grammar.py
@@ -1,6 +1,7 @@
 from compila.constants import EPSILON
 from compila.parser.grammar import Grammar
 from compila.parser.production import Production
+from itertools import count
 
 
 class TestLangGrammar(Grammar):
@@ -14,33 +15,33 @@ class TestLangGrammar(Grammar):
         # ou a próxima label mas elas deveriam ser restritas a cada parser
         # e para cada execução então era bom pensar um pouco sobre como fazer isso
         productions = [
-            Production("IF_STMT", [
-                "if",
-                "open_parentheses",
-                "CONDITION",
-                "close_parentheses",
-                "open_curly_brackets",
-                "EXPRESSION",
-                "close_curly_brackets",
-                op_if_code_0
-            ]),
-            Production("CONDITION", ["number", op_if_code_1]),
+            # Production("IF_STMT", [
+            #     "if",
+            #     "open_parentheses",
+            #     "CONDITION",
+            #     "close_parentheses",
+            #     "open_curly_brackets",
+            #     "EXPRESSION",
+            #     "close_curly_brackets",
+            #     op_if_code_0
+            # ]),
+            # Production("CONDITION", ["number", op_if_code_1]),
 
-            Production("EXPRESSION", [op_expr_code_0, "E", op_expr_code_1]),
+            Production("EXPRESSION", [op_expr_code_start, "E", op_expr_code_end]),
 
-            Production("E", [op_expr_code_2, "T", op_expr_code_4, "E1", op_expr_code_5]),
-            Production("E1", ["add", op_expr_code_6, "T", op_expr_code_sum, "E1", op_expr_code_8]),
-            Production("E1", ["sub", op_expr_code_6, "T", op_expr_code_sub, "E1", op_expr_code_8]),
-            Production("E1", [EPSILON, op_expr_code_9]),
+            Production("E", [op_expr_code_0, "T", op_expr_code_1, "E1", op_expr_code_2]),
+            Production("E1", ["add", op_expr_code_3, "T", op_expr_code_4, "E1", op_expr_code_add]),
+            Production("E1", ["sub", op_expr_code_3, "T", op_expr_code_4, "E1", op_expr_code_sub]),
+            Production("E1", [EPSILON, op_expr_code_empty]),
 
-            Production("T", [op_expr_code_2, "F", op_expr_code_4, "T1", op_expr_code_5]),
-            Production("T1", ["mul", op_expr_code_6, "F", op_expr_code_mul, "T1", op_expr_code_8]),
-            Production("T1", ["div", op_expr_code_6, "F", op_expr_code_div, "T1", op_expr_code_8]),
-            Production("T1", [EPSILON, op_expr_code_9]),
+            Production("T", [op_expr_code_0, "F", op_expr_code_1, "T1", op_expr_code_2]),
+            Production("T1", ["mul", op_expr_code_3, "F", op_expr_code_4, "T1", op_expr_code_mul]),
+            Production("T1", ["div", op_expr_code_3, "F", op_expr_code_4, "T1", op_expr_code_div]),
+            Production("T1", [EPSILON, op_expr_code_empty]),
 
-            Production("F", ["identifier", op_expr_code_3]),
-            Production("F", ["number", op_expr_code_7]),
-            Production("F", ["open_parentheses", op_expr_code_10, "E", op_expr_code_11, "close_parentheses"])
+            Production("F", ["identifier", op_expr_code_identifier]),
+            Production("F", ["number", op_expr_code_identifier]),
+            Production("F", ["open_parentheses", op_expr_code_open_parentheses, "E", op_expr_code_close_parentheses, "close_parentheses"]),
         ]
 
         return productions
@@ -62,74 +63,66 @@ def op_if_code_1(condition, number):
     condition.syn_code = f"t0 = {number.token.lexema} - 5\n"
 
 # Semantic rules to generate code for expressions
-def op_expr_code_0(expression, e):
-    e.her_var = -1
-    expression.syn_label = 1
-    e.her_code = f"L{expression.syn_label}:\n"
+def op_expr_code_start(expression, e):
+    e.var_counter = count()
 
-def op_expr_code_1(expression, e):
-    expression.syn_code = e.syn_code
-    expression.syn_var = e.syn_var
+def op_expr_code_end(expression, e):
+    expression.code = e.code
+
+def op_expr_code_0(e, t, e_dash):
+    t.var_counter = e.var_counter
+    e_dash.var_counter = e.var_counter
+
+def op_expr_code_1(e, t, e_dash):
+    e_dash.her_var_2 = t.var
 
 def op_expr_code_2(e, t, e_dash):
-    t.her_var = e.her_var
-    t.her_code = e.her_code
+    e.code = t.code + e_dash.code
+    e.var = e_dash.var
 
-def op_expr_code_3(t, identifier):
-    t.syn_var = t.her_var + 1
-    code = f"t{t.syn_var} = {identifier.token.lexema}\n"
-    t.syn_code = t.her_code + code
+def op_expr_code_3(e_dash_0, _, t, e_dash_1):
+    t.var_counter = e_dash_0.var_counter
+    e_dash_1.var_counter = e_dash_0.var_counter
 
-def op_expr_code_4(e, t, e_dash):
-    e_dash.her_var = t.syn_var
-    e_dash.her_code = t.syn_code
-
-def op_expr_code_5(e, t, e_dash):
-    e.syn_var = e_dash.syn_var
-    e.syn_code = e_dash.syn_code
-
-def op_expr_code_6(e_dash_0, _, t, e_dash_1):
-    t.her_var = e_dash_0.her_var
-    t.her_code = e_dash_0.her_code
-
-def op_expr_code_7(t, number):
-    t.syn_var = t.her_var + 1
-    code = f"t{t.syn_var} = {number.token.lexema}\n"
-    t.syn_code = t.her_code + code
-
-def op_expr_code_8(e_dash_0, _, t, e_dash_1):
-    e_dash_0.syn_var = e_dash_1.syn_var
-    e_dash_0.syn_code = e_dash_1.syn_code
-
-def op_expr_code_9(e_dash, _):
-    e_dash.syn_var = e_dash.her_var
-    e_dash.syn_code = e_dash.her_code
-
-def op_expr_code_10(f, _0, e, _1):
-    e.her_var = f.her_var
-    e.her_code = f.her_code
-
-def op_expr_code_11(f, _0, e, _1):
-    f.syn_var = e.syn_var
-    f.syn_code = e.syn_code
+def op_expr_code_4(e_dash_0, _, t, e_dash_1):
+    e_dash_1.her_var_2 = t.var
 
 # 
-def op_expr_code_sum(e_dash_0, _, t, e_dash_1):
-    e_dash_1.her_var = t.syn_var + 1
-    code = f"t{e_dash_1.her_var} = t{e_dash_0.her_var} + t{t.syn_var}\n"
-    e_dash_1.her_code = t.syn_code + code
+def op_expr_code_identifier(f, number):
+    f.var = f"t{next(f.var_counter)}"
+    f.code = f"{f.var} = {number.token.lexema}\n"
+
+def op_expr_code_empty(e_dash, _):
+    e_dash.code = ""
+    e_dash.var = e_dash.her_var_2
+
+# 
+def op_expr_code_number(t, number):
+    t.var = f"t{next(t.var_counter)}"
+    t.code = f"{t.var} = {number.token.lexema}\n"
+
+def op_expr_code_open_parentheses(f, _0, e, _1):
+    e.var_counter - f.var_counter
+
+def op_expr_code_close_parentheses(f, _0, e, _1):
+    f.code = e.code
+# 
+def op_expr_code_add(e_dash_0, _, t, e_dash_1):
+    e_dash_0.var = f"t{next(e_dash_0.var_counter)}"
+    op_code = f"{e_dash_0.var} = {e_dash_0.her_var_2} + {e_dash_1.var}\n"
+    e_dash_0.code = t.code + e_dash_1.code + op_code
 
 def op_expr_code_sub(e_dash_0, _, t, e_dash_1):
-    e_dash_1.her_var = t.syn_var + 1
-    code = f"t{e_dash_1.her_var} = t{e_dash_0.her_var} - t{t.syn_var}\n"
-    e_dash_1.her_code = t.syn_code + code
+    e_dash_0.var = f"t{next(e_dash_0.var_counter)}"
+    op_code = f"{e_dash_0.var} = {e_dash_0.her_var_2} - {e_dash_1.var}\n"
+    e_dash_0.code = t.code + e_dash_1.code + op_code
 
 def op_expr_code_mul(t_dash_0, _, f, t_dash_1):
-    t_dash_1.her_var = f.syn_var + 1
-    code = f"t{t_dash_1.her_var} = t{t_dash_0.her_var} * t{f.syn_var}\n"
-    t_dash_1.her_code = f.syn_code + code
+    t_dash_0.var = f"t{next(t_dash_0.var_counter)}"
+    op_code = f"{t_dash_0.var} = {t_dash_0.her_var_2} * {t_dash_1.var}\n"
+    t_dash_0.code = f.code + t_dash_1.code + op_code
 
 def op_expr_code_div(t_dash_0, _, f, t_dash_1):
-    t_dash_1.her_var = f.syn_var + 1
-    code = f"t{t_dash_1.her_var} = t{t_dash_0.her_var} / t{f.syn_var}\n"
-    t_dash_1.her_code = f.syn_code + code
+    t_dash_0.var = f"t{next(t_dash_0.var_counter)}"
+    op_code = f"{t_dash_0.var} = {t_dash_0.her_var_2} / {t_dash_1.var}\n"
+    t_dash_0.code = f.code + t_dash_1.code + op_code
--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from compila.tac_interpreter import TACInterpreter
 
 parser = TestLangParser()
 
-origin_code = "if (2) {5 + 5 - 2}"
-# origin_code = "2 + 3 * 5"
+# origin_code = "if (2) {5 + 5 - 2}"
+origin_code = "a + b * c + d / 2"
 
 bla = parser.analyze(origin_code)
-inter_code = bla.syn_code + "print t0"
-print(inter_code)
+# inter_code = bla.syn_code + "print t0"
+print(bla.code)
 
-interpreter = TACInterpreter()
-interpreter.run(inter_code)
+# print(inter_code)
+
+# interpreter = TACInterpreter()
+# interpreter.run(inter_code)

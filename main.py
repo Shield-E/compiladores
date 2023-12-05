130bd852d5a749a879a4065a91603b6ab3432ed1
André Fernandes <fpf.andre@gmail.com>
2023-12-05 11:01:57 -0300

    break input in multiple lines

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ from compila.tac_interpreter import TACInterpreter
 
 parser = CCLangParser()
 
-origin_code = "def matrixmult(int matA, int matB){ for(i = 0; i < 6; i = i + 1) for(j = 0; j < 6; j = j + 1) int matResult[5][5]; matResult[i][j] = 0; for(k = 0; k < 6; k = k + 1) matResult[i][j] = matResult[i][j] + matA[i][k] * matB[k][j]; print matResult; return;}"
+origin_code = '''
+def matrixmult(int matA, int matB) {
+    for(i = 0; i < 6; i = i + 1) 
+        for(j = 0; j < 6; j = j + 1) 
+            int matResult[5][5];
+            matResult[i][j] = 0;
+            for(k = 0; k < 6; k = k + 1) 
+                matResult[i][j] = matResult[i][j] + matA[i][k] * matB[k][j];
+                print matResult; return;
+}
+'''
 
 bla = parser.analyze(origin_code)
 inter_code = bla.code + "print bla"
